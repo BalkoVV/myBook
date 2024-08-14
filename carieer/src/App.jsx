e92ef34dd0a561348d060840a19c2953f71bc827
ViktorBalko <ben.happyngton@gmail.com>
2024-08-15 02:48:55 +0300

    #1508-0248
    #1

---
+++ carieer/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+
+      <section className='hero'>
+<h1>Якщо Вашого бізнесу немає в Інтернеті..</h1>
+<h1>..у Вас немає Бізнесу</h1>
+      </section>
+     {/* <section className='Book'>
+
+        <div className='first'>
+
+        </div>
+
+        <div className='second'>
+
+        </div>
+
+     </section> */}
+    </div>
+  );
+}
+
+export default App;
